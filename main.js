6931eee1b641f0e3e05eeed40fd87ef231589296
adriel <adriel@taqe.com.br>
2023-07-03 22:09:07 -0300

    finalização do curso

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 function tocaSom(idElement){
-    document.querySelector(idElement).play();
+    const elemento = document.querySelector(idElement)
+
+    if(elemento != null && elemento.localName === 'audio'){
+            elemento.play();
+        }else{
+            console.log('objeto não encontrado ou não é um audio')
+        }
 }
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 for(let i = 0 ; i < listaTeclas.length; i++){
-    const tecla = listaTeclas[contador]
-
+    
+    const tecla = listaTeclas[i]
     const instrumento = tecla.classList[1];
-
-    console.log(instrumento);
+    const idAudio = `#som_${instrumento}`
 
     tecla.onclick = function (){
-        tocaSom(`#som_${instrumento}` );
-        
-    };
-    contador = contador + 1;
-    console.log(contador);
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento) {
+        if(evento.code == 'Enter' || evento.code == "Space"){
+            tecla.classList.add('ativa')
+        }
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
 }
